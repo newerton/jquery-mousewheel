4ad8bb8d6b1b0858eb98fa480bcb589f4cd3909e
Brandon Aaron <hello.brandon@aaron.sh>
2013-11-14 11:51:03 -0500

    Add the trigger hook to actually have a functioning trigger

--- jquery.mousewheel.js
+++ jquery.mousewheel.js
@@ -67,11 +67,23 @@
 
         settings: {
             adjustOldDeltas: true
+        },
+
+        trigger: function(data, event) {
+          if (!event) {
+            event = data;
+            data = null
+          }
+
+          handler.call(this, event);
+
+          return false;
         }
     };
 
     function handler(event) {
-        var orgEvent   = event || window.event,
+        // might be trigged event, so check for the originalEvent first
+        var orgEvent   = event ? event.originalEvent || event : window.event,
             args       = slice.call(arguments, 1),
             delta      = 0,
             deltaX     = 0,
